aa33a091ba2bd3878ccc2e784a5cca6e0444f80d
Mai <myang012@gmail.com>
2024-03-20 13:40:17 -0500

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,21 @@
 const summary = [];
 const fitnessTracker = [
-    {name: "Interval Training", time: 30 },
-    {name: "Squats", time: 30 },
+    {name: "Interval Training", time: 40 },
+    {name: "Squats", time: 20 },
     {name: "Bent-Over-Row", time: 15 },
-    {name: "Upright Row", time: 15 }
+    {name: "Upright Row", time: 15 },
 
-];
+    ];
+    const fitnessTimes = [20, 30, 50];
+
+    //Estimated times for Task 1
+
+//Task 1: Calculate Total Fitness Times
+    function calculateTotalTimes(time){
+        return time.reduce((total, time) => total + time, 0);
+    };
+
+//Task 2: Filter New Workout Class
+    function filterNewClasses(workouts){
+        return 
+    }
